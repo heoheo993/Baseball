3d2b2c03f19dc62f597f3b4acfc862ecd9e88e16
beomgyu.heo <0105086@naver.com>
2024-05-17 14:36:42 +0900

    Commit

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -4,8 +4,17 @@ using namespace std;
 class Baseball
 {
 public:
-	void guess(const string& str)
+	void guess(const string& guessNumber)
 	{
-		throw length_error("Must be three letters.");
+		if (guessNumber.length() != 3)
+		{
+			throw length_error("Must be three letters.");
+		}
+		for (char ch : guessNumber)
+		{
+			if (ch < '0' || ch>'9') {
+				throw invalid_argument("Must be number");
+			}
+		}
 	}
 };
--- Sample-Test-BaseBall/baseball_test.cpp
+++ Sample-Test-BaseBall/baseball_test.cpp
@@ -1,7 +1,27 @@
 #include "pch.h"
 #include "../BaseBall/baseball.cpp"
-TEST(BaseballGame, ThrowExceptionWhenInputlength_error) {
+class BaseballFixture : public testing::Test
+{
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess("123"), length_error);
-
+	void assertillegalArgument(string guessNumber)
+	{
+		try
+		{
+			game.guess(guessNumber);
+			
+		}
+		catch (exception e)
+		{
+			FAIL();
+		}
+	}
+};
+TEST_F(BaseballFixture, ThrowExceptionWhenInputlength_error)
+{
+	assertillegalArgument(string("14s"));
+}
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalicChar)
+{
+	assertillegalArgument(string("13"));
 }
